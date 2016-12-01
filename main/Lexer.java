de73443bfd988a0e238b51d2f56c87afdea09884
Thomas Barrett <tbarrett1200@icloud.com>
2016-12-01 14:32:18 -0500

    add regex lexer

--- main/Lexer.java
+++ main/Lexer.java
@@ -1,62 +1,35 @@
 package main;
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lexer {
 
-	private InputStream in;
 	private ArrayList<String> tokens = new ArrayList<String>();
 	private int index = 0;
 	
-	public Lexer(InputStream in) {
-		
-		this.in = in;
-		String current = "";
-		char c = 0;
-		
-		boolean getChar = true;
-		
-		try {
-			while(true) {
-				current = "";
-				
-				c = getChar? getChar() : c;
-				getChar = true;
-				
-				if (Character.isWhitespace(c)) continue;
-			
-				
-				if (Character.isJavaIdentifierStart(c)) {
-					current += c;
-					c = getChar();
-					while (Character.isJavaIdentifierPart(c)) {
-						current += c;
-						c = getChar();
-					}
-					getChar = false;
-					tokens.add(current);
-					continue;
-				}
-				
-				if (Character.isDigit(c)) {
-					while (Character.isDigit(c)) {
-						current += c;
-						c = getChar();
-					}
-					getChar = false;
-					tokens.add(current);
-					continue;
-				}
-				
-				current+=c;
-				tokens.add(current);
-				
-			}
-		} catch (EOFException e) {
-			if (!current.isEmpty()) tokens.add(current);
+	private Pattern identifier = Pattern.compile("[a-zA-Z][a-zA-Z0-9]*");
+	private Pattern number = Pattern.compile("[0-9]+");
+	private Pattern operator = Pattern.compile("[+-/%=;]");
+	private Pattern space = Pattern.compile(" ");
+	private Pattern[] patterns = {identifier, number, space, operator};
+	
+	public Lexer(String file) {
+	    Matcher match = Pattern.compile("").matcher(file);
+	    int start = 0;
+	    loop: while(true) {
+		if (start==file.length()) break;
+		for (Pattern pattern: patterns) {
+		    match.usePattern(pattern);
+		    if (match.find(start) && match.start()==start) {
+			if (match.pattern()!=space) tokens.add(match.group());
+			start = match.end();
+			continue loop;
+		    }
 		}
+		System.out.println("Lexer Error");
+		break;
+	    }
 	}
 	
 	public void list() {
@@ -68,6 +41,7 @@ public class Lexer {
 	public boolean hasNext(String s) {
 		if (index == tokens.size()) return false;
 		return tokens.get(index).matches(s);
+		
 	}
 	
 	public boolean hasNext() {
@@ -78,12 +52,5 @@ public class Lexer {
 	public String next() {
 		return tokens.get(index++);
 	}
-	private char getChar() throws EOFException {
-		int c = 0;
-		try { 
-			c = in.read();
-		} catch (IOException e) { e.printStackTrace(); }
-		if (c == -1) throw new EOFException();
-		return (char)c;
-	}
+	
 }
